74b50279b211f67035439a01d98c615dad593acb
Diemlykt <143418329+Diemlykt@users.noreply.github.com>
2023-11-10 10:49:30 +0700

    Update st.py

--- st.py
+++ st.py
@@ -1,4 +1,5 @@
-pip install -U scikit-learn
+import subprocess
+subprocess.run(["pip", "install", "scikit-learn"])
 import streamlit as st
 import pandas as pd
 from sklearn.impute import SimpleImputer
