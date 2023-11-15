8ac0a57e32662f681bbdfd80d8966aa4aca61188
Diemlykt <143418329+Diemlykt@users.noreply.github.com>
2023-11-15 10:39:27 +0700

    Update st.py

--- st.py
+++ st.py
@@ -1,17 +1,18 @@
+# app.py
 import streamlit as st
 import pandas as pd
 import numpy as np
-from sklearn.impute import SimpleImputer
 import sklearn.preprocessing as pre
-from sklearn.model_selection import train_test_split
+from sklearn.impute import SimpleImputer
 from sklearn.ensemble import RandomForestRegressor
+import joblib
+
+# Load the saved model and preprocessing parameters
+lr_model = joblib.load('lr_model.joblib')
+features_columns = joblib.load('features_columns.joblib')
 
 
-
-data = pd.read_csv("survey_results_public.csv")
-
-
-# Title and Sidebar Title
+# Streamlit app code
 st.title("2023 Developer Salary Web App")
 st.sidebar.title("2023 Developer Salary Web App")
 
@@ -78,40 +79,36 @@ def user_input_features():
 # Creating a DataFrame
     data_predict = pd.DataFrame(features, index=index)
     return data_predict
-
 def pre_process(filtered_df):
-   learn_code=filtered_df['LearnCode'].str.get_dummies(sep=';')
-   languages = filtered_df['LanguageHaveWorkedWith'].str.get_dummies(sep=';')
-   Employ_situation = filtered_df['Employment'].str.get_dummies(sep=';')
-   Merge_data=filtered_df.merge(Employ_situation,left_index=True, right_index=True).merge(languages,left_index=True, right_index=True).merge(learn_code,left_index=True, right_index=True)
-   New_data=Merge_data.drop(columns=["Employment", "LearnCode", "LanguageHaveWorkedWith"])
-   table = New_data.copy()
-   wanted_cols = table.select_dtypes(include=['object']).columns
-   encoders = [pre.LabelEncoder()]*len(wanted_cols)
-   colname2encoder = dict(zip(wanted_cols, encoders))
+    learn_code=filtered_df['LearnCode'].str.get_dummies(sep=';')
+    languages = filtered_df['LanguageHaveWorkedWith'].str.get_dummies(sep=';')
+    Employ_situation = filtered_df['Employment'].str.get_dummies(sep=';')
+    Merge_data=filtered_df.merge(Employ_situation,left_index=True, right_index=True).merge(languages,left_index=True, right_index=True).merge(learn_code,left_index=True, right_index=True)
+    New_data=Merge_data.drop(columns=["Employment", "LearnCode", "LanguageHaveWorkedWith"])
+    table = New_data.copy()
+    wanted_cols = table.select_dtypes(include=['object']).columns
+    encoders = [pre.LabelEncoder()]*len(wanted_cols)
+    colname2encoder = dict(zip(wanted_cols, encoders))
    # Transform columns
-   for colname in colname2encoder.keys():
-      table[colname] = colname2encoder[colname].fit_transform(table[colname])
+    for colname in colname2encoder.keys():
+        table[colname] = colname2encoder[colname].fit_transform(table[colname])
         
    # Create an imputer object and specify the imputation strategy
-   imputer = SimpleImputer(strategy="most_frequent")
+    imputer = SimpleImputer(strategy="most_frequent")
 
    # Fit the imputer to the data
-   imputer.fit(table)
+    imputer.fit(table)
 
    # Transform the data to impute missing values
-   table_transformed = imputer.transform(table)
+    table_transformed = imputer.transform(table)
 
    # Convert the result back to a DataFrame
-   table_transformed = pd.DataFrame(table_transformed, columns=table.columns)
-   return table_transformed
-    
+    table_transformed = pd.DataFrame(table_transformed, columns=table.columns)
+    return table_transformed
 
 
 
-# Example usage
 
-# Display content based on user selection
 if app_mode == "Salary Analysis":
     st.markdown("You've selected Salary Analysis.")
     # Add code for salary analysis here
@@ -119,32 +116,25 @@ if app_mode == "Salary Analysis":
 elif app_mode == "Predict Salary":
     st.markdown("You've selected Predict Salary.")
     # Add code for salary prediction here
-    df=user_input_features()
+    df = user_input_features()
     st.subheader("User input parameters")
     st.write(df)
-    select=['Age', 'Employment', 'RemoteWork', 'EdLevel', 'LearnCode', 'YearsCodePro','Industry', 'DevType', 'OrgSize','ConvertedCompYearly', 'Country', 'LanguageHaveWorkedWith']
-    data_df =data[select]
-    df_no_missing = data_df.dropna(subset='ConvertedCompYearly')
-    y = np.log(df_no_missing['ConvertedCompYearly']).copy()
-    filtered_df = df_no_missing.drop(columns=['ConvertedCompYearly'])
-    X = pre_process(filtered_df)
-    
-    X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.2, random_state=42)
-    rf_model = RandomForestRegressor(n_estimators=100, random_state=42)  # You can adjust the hyperparameters
-    rf_model.fit(X_train, y_train)
-    X_user=pre_process(df)
-    missing_columns = set(X.columns) - set(X_user.columns)
+
+    # Preprocess user input
+    X_user = pre_process(df)
+
+    # Ensure the input features have the same columns as the model was trained on
+    missing_columns = set(features_columns) - set(X_user.columns)
     for col in missing_columns:
-       X_user[col] = 0
-    X_user = X_user.reindex(columns=X.columns, fill_value=0)
-    y_pred = rf_model.predict(X_user)
+        X_user[col] = 0
+    X_user = X_user.reindex(columns=features_columns, fill_value=0)
+
+    # Make prediction
+    y_pred = lr_model.predict(X_user)
     y_pred_original_scale = np.exp(y_pred)
     formatted_salary = "${:,.2f}".format(y_pred_original_scale[0])
 
 if st.button("Predict salary", key="predict"):
     st.markdown("Your salary prediction is:")
     st.write(formatted_salary)
-
-
 
